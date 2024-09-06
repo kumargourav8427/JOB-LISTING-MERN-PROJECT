636c6c189917625fc607c0f8b7ad0a26ea7e0533
kumargourav8427 <84629875+kumargourav8427@users.noreply.github.com>
2024-09-06 15:10:32 +0530

    add some folder name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./layouts/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Home/>
     </>
   );
 }
