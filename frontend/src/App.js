e85c004516874fb9f52ba671f2529a382556397b
kumargourav8427 <84629875+kumargourav8427@users.noreply.github.com>
2024-09-07 16:14:17 +0530

    page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import Navbar from "./layouts/Navbar";
 import Home from "./Pages/Home";
+import Blogs from "./Pages/Blogs";
+import Contact from "./Pages/Contact";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import JobListing from "./Pages/JobListing";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="/job-listing" element={<JobListing />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
--- frontend/src/layouts/Navbar.js
+++ frontend/src/layouts/Navbar.js
@@ -1,20 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <nav className='flex justify-around bg-black text-white h-12 items-center'>
-        <div>
-                <img src='logo' alt='logo'/>
-        </div>
-        <div>
-            <ul className='flex gap-10 '>
-                <li className='cursor-pointer hover:font-bold'>Home</li>
-                <li className='cursor-pointer hover:font-bold'>About</li>
-                <li className='cursor-pointer hover:font-bold'>Services</li>
-                <li className='cursor-pointer hover:font-bold'>Login</li>
-                <li className='cursor-pointer hover:font-bold'>SignUp</li>
-            </ul>
-        </div>
+      <div>
+        <img src='logo' alt='logo' />
+      </div>
+      <div>
+        <ul className='flex gap-10 '>
+          <li>
+            <Link to="/" className='cursor-pointer hover:font-bold'>Home</Link>
+          </li>
+          <li>
+            <Link to="/job-listing" className='cursor-pointer hover:font-bold'>Job</Link>
+          </li>
+          <li>
+            <Link to="/blogs" className='cursor-pointer hover:font-bold'>Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact" className='cursor-pointer hover:font-bold'>Contact</Link>
+          </li>
+
+        </ul>
+      </div>
     </nav>
   )
 }
