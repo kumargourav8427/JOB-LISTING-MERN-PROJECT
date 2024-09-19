fa94bf051307a500021fa28e36c76dc6af722e9d
Pratyush112 <prasaha003@gmail.com>
2024-09-19 18:30:02 +0530

    Updated home page

--- frontend/src/Pages/Job.js
+++ frontend/src/Pages/Job.js
@@ -7,83 +7,163 @@ function Job() {
         <div class="container px-5 py-24 mx-auto">
           <div class="flex flex-wrap -m-4">
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                <p class="mt-1">$16.00</p>
+              <div className="card bg-base-100 w-90 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 ...">
+                <figure className="px-10 pt-10">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBqXa-HzaumlIW1g15CR-wSnJGfNzI0DWBBg&s"
+                    alt="Microsoft"
+                    className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Microsoft</h2>
+                  <p>Role - Full Stack Developer</p>
+                  <p>Number of openings - 10</p>
+                  <p>Location - Delhi</p>
+                  <p>Rs4,00,000 - Rs4,50,000</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Apply Now</button>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261" />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                <p class="mt-1">$21.15</p>
+              <div className="card bg-base-100 w-90 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 ...">
+                <figure className="px-10 pt-10">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDCszCzX7-i49NW_W5CjuLDWfUOB84Y-CBBg&s"
+                    alt="Trustt"
+                    className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Trustt</h2>
+                  <p>Role - Data Scientist</p>
+                  <p>Number of openings - 7</p>
+                  <p>Work from home</p>
+                  <p>Rs6,00,000 - Rs6,50,000</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Apply Now</button>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262" />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                <p class="mt-1">$12.00</p>
+              <div className="card bg-base-100 w-90 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 ...">
+                <figure className="px-10 pt-10">
+                  <img
+                    src="https://connect-assets.prosple.com/cdn/ff/n8jJsTCiC7Ra1H3MrBX-x3P-n9zZq4Sfs026oUM-6V0/1631623083/public/styles/scale_and_crop_center_224x224/public/2021-09/Logo-DeltaX%20India%20-480x480-2021.jpg?itok=gzFwZAg8"
+                    alt="IBM"
+                    className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">DeltaX</h2>
+                  <p>Role - Content Writer</p>
+                  <p>Number of openings - 5</p>
+                  <p>Work From Home</p>
+                  <p>Rs5,00,000 - Rs7,50,000</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Apply Now</button>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263" />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                <p class="mt-1">$18.40</p>
+              <div className="card bg-base-100 w-90 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 ...">
+                <figure className="px-10 pt-10">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Logo_Cognizant.png"
+                    alt="Cognizant"
+                    className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Cognizant</h2>
+                  <p>Role - Backend Developer</p>
+                  <p>Number of openings - 8</p>
+                  <p>Location - Lucknow</p>
+                  <p>Rs4,00,000 - Rs5,50,000</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Apply Now</button>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264" />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                <p class="mt-1">$16.00</p>
+              <div className="card bg-base-100 w-90 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 ...">
+                <figure className="px-10 pt-10">
+                  <img
+                    src="https://gumlet-images.assettype.com/afaqs/import/all/news/images/news_story_grfx/2017/50354_1_home_big.jpg?auto=format,compress&fmt=webp&format=webp&w=1200&h=900&dpr=1.0"
+                    alt="Wipro"
+                    className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Wipro</h2>
+                  <p>Role - React JS Developer</p>
+                  <p>Number of openings - 12</p>
+                  <p>Location - Kolkata</p>
+                  <p>Rs3,00,000 - Rs4,50,000</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Apply Now</button>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265" />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                <p class="mt-1">$21.15</p>
+              <div className="card bg-base-100 w-90 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 ...">
+                <figure className="px-10 pt-10">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnsOsKYLc1HIt44m97SBW7YLlCqSNV8Qrp0Q&s"
+                    alt="Google"
+                    className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Google</h2>
+                  <p>Role - Artificial Intelligence</p>
+                  <p>Number of openings - 12</p>
+                  <p>Location - Gurgaon</p>
+                  <p>Rs3,00,000 - Rs5,50,000</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Apply Now</button>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267" />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                <p class="mt-1">$12.00</p>
+              <div className="card bg-base-100 w-90 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 ...">
+                <figure className="px-10 pt-10">
+                  <img
+                    src="https://i.pinimg.com/736x/2d/10/c5/2d10c523ae08de00403a601fa91018e9.jpg"
+                    alt="Deloitte"
+                    className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Deloitte</h2>
+                  <p>Role - Graphics Designer</p>
+                  <p>Number of openings - 7</p>
+                  <p>Location - Hyderabad</p>
+                  <p>Rs3,00,000 - Rs3,50,000</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Apply Now</button>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a class="block relative h-48 rounded overflow-hidden">
-                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268" />
-              </a>
-              <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                <p class="mt-1">$18.40</p>
+              <div className="card bg-base-100 w-90 shadow-xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-300 ...">
+                <figure className="px-10 pt-10">
+                  <img
+                    src="https://app-stockarea.s3.ap-south-1.amazonaws.com/images/SEO-12-0.2020-09-03%2018%3A31%3A09.jpg"
+                    alt="Stockarea"
+                    className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Stockarea</h2>
+                  <p>Role - Front End Developer</p>
+                  <p>Number of openings - 7</p>
+                  <p>Location - Chennai</p>
+                  <p>Rs12,00,000 - Rs16,00,000</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Apply Now</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
