5d48f4fd1c4b3c6ccba7f2fffc47770bb2900b25
kumargourav8427 <84629875+kumargourav8427@users.noreply.github.com>
2024-09-21 15:39:04 +0530

    home job footer addded by kumar saha and I change in auth

--- frontend/src/auth/Login.js
+++ frontend/src/auth/Login.js
@@ -1,97 +1,99 @@
 import React, { useState } from 'react';
 import './auth.css';
 import axios from 'axios'
-import {useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { USER_API_END_POINT } from '../apiAddress/constant';
 
 
 const Login = () => {
-  const [loginInput,setLoginInput] = useState({
-    email:"",
-    password:"",
-    role:""
+  const [loginInput, setLoginInput] = useState({
+    email: "",
+    password: "",
+    role: ""
   })
 
-  const changeEventHandler = (e) =>{
-    setLoginInput({...loginInput,[e.target.name]:e.target.value})
+  const changeEventHandler = (e) => {
+    setLoginInput({ ...loginInput, [e.target.name]: e.target.value })
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try{
-      const res  = await axios.post(`${USER_API_END_POINT}/login`,loginInput,{
-        headers:{"Content-Type":"application/json"},
-        withCredentials :true
+    try {
+      const res = await axios.post(`${USER_API_END_POINT}/login`, loginInput, {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true
       })
-      console.log(res.data.success,"hgsdhfgds");
-      
-      if(res.data.success){
+      console.log(res.data.success, "hgsdhfgds");
+
+      if (res.data.success) {
         navigate("/")
       }
-    }catch(error){
+    } catch (error) {
       console.log(error);
-      
+
 
     }
-    
+
     // console.log('Login:', { email, password });
-    console.log(loginInput,"login data");
-    
+    console.log(loginInput, "login data");
+
   };
 
   const navigate = useNavigate()
 
   return (
-    <div className='main-auth'>
-    <div className="auth-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={loginInput.email}
-            onChange={changeEventHandler}
-            placeholder='Enter email '
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={loginInput.password}
-            onChange={changeEventHandler}
-            placeholder='Enter email '
+    <div className='main-auth py-5 min-[100vh] flex justify-center'>
+      <div className="auth-container">
+        <h2 className='text-3xl font-bold'>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={loginInput.email}
+              onChange={changeEventHandler}
+              placeholder='Enter email '
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={loginInput.password}
+              onChange={changeEventHandler}
+              placeholder='Enter email '
 
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Student</label>
-          <input
-            type="radio"
-            name="role"
-            value="student"
-            checked={loginInput.role === 'student'}
-            onChange={changeEventHandler}
-            className='cursor-pointer'
-          />
-           <label htmlFor="password">Recruiter</label>
-          <input
-            type="radio"
-            name="role"
-            value="recruiter"
-            checked={loginInput.role === 'recruiter'}
-            onChange={changeEventHandler}
-            className='cursor-pointer'
+            />
+          </div>
+          <div className="form-group flex">
+            <label htmlFor="password">Student</label>
+            <input
+              type="radio"
+              name="role"
+              value="student"
+              checked={loginInput.role === 'student'}
+              onChange={changeEventHandler}
+              className='cursor-pointer'
+            />
+            <label htmlFor="password">Recruiter</label>
+            <input
+              type="radio"
+              name="role"
+              value="recruiter"
+              checked={loginInput.role === 'recruiter'}
+              onChange={changeEventHandler}
+              className='cursor-pointer'
 
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+            />
+          </div>
+          <button type="submit" className='mb-3'>Login</button>
+          <p className="text-red-800 text-center">Don't have an account <Link to ="/signup" className="text-blue-900">Signup</Link></p>
+
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/auth/Signup.js
+++ frontend/src/auth/Signup.js
@@ -1,73 +1,118 @@
-import React, { useState } from 'react';
-import './auth.css';
+import React, { useState } from "react";
+import "./auth.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { USER_API_END_POINT } from "../apiAddress/constant";
 
 const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [name, setName] = useState('');
-  const [error, setError] = useState('');
+  const [signupInput, setSignupInput] = useState({
+    fullanme: "",
+    email: "",
+    password: "",
+    role: "",
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      setError("Passwords do not match!");
-      return;
-    }
-    // Handle signup logic here
-    console.log('Signup:', { name, email, password });
-    setError('');
+  const changeEventHandler = (e) => {
+    setSignupInput({ ...signupInput, [e.target.name]: e.target.value });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        `${USER_API_END_POINT}/register`,
+        signupInput,
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(res.data.success, "sinup success");
+
+      if (res.data.success) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(signupInput, "signup data");
+  };
+
+  const navigate = useNavigate();
+
   return (
-    <div className='main-auth'>
-    <div className="auth-container">
-      <h2>Signup</h2>
-      {error && <p className="error-message">{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Full Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="confirmPassword">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Signup</button>
-      </form>
-    </div>
+    <div className="main-auth py-3 min-[100vh] flex justify-center">
+      <div className="auth-container ">
+        <h2 className="text-3xl font-bold">Signup</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Full Name:</label>
+            <input
+              type="text"
+              name="fullname"
+              value={signupInput.fullanme}
+              onChange={changeEventHandler}
+              placeholder="Enter fullname"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={signupInput.fullanme}
+              onChange={changeEventHandler}
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={signupInput.fullanme}
+              onChange={changeEventHandler}
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              type="password"
+              name="confirm_pass"
+              value={signupInput.fullanme}
+              onChange={changeEventHandler}
+              placeholder="Re-Enter your password"
+            />
+          </div>
+          <div className="form-group flex">
+            <label htmlFor="password">Student</label>
+            <input
+              type="radio"
+              name="role"
+              value="student"
+              checked={signupInput.role === "student"}
+              onChange={changeEventHandler}
+              className="cursor-pointer"
+            />
+            <label htmlFor="password">Recruiter</label>
+            <input
+              type="radio"
+              name="role"
+              value="recruiter"
+              checked={signupInput.role === "recruiter"}
+              onChange={changeEventHandler}
+              className="cursor-pointer"
+            />
+          </div>
+          <button type="submit" className='mb-3'>Signup</button>
+          <p className="text-red-800 text-center">
+          Alredy have an account <Link to="/login" className="text-blue-900">Login</Link>
+
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
