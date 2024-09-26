46341d92e0693104b362db97296bb6b9d910ee0b
kumargourav8427 <84629875+kumargourav8427@users.noreply.github.com>
2024-09-26 18:04:56 +0530

    remove profile added by me

--- frontend/src/admin/CreateJob.js
+++ frontend/src/admin/CreateJob.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function CreateJob() {
+  const navigate = useNavigate()
+const registerNewComapny = async() =>{
+  try{
+
+  }
+  catch(error){
+
+  }
+}
   return (
     <div>
         <div className='max-w-4xl mx-auto'>
             <h1 className='font-bold text-2xl'>Your Company Name</h1>
-            <p>lorem45</p>
+            <p>What should br craete the company abput his </p>
+            <div className='flex flex-col py-5'>
+              <label className='py-3'>Company Name</label>
+              <input type='text' 
+              className='p-3 border-solid border-4 border-black'
+              placeholder='Job Hunt Microsoft etc.'
+              />
+            </div>
+            <div className='flex gap-5 w-fit'>
+              <button onClick={()=> navigate("/admin/company")}>Cancel</button>
+              <button className='bg-black text-white hover:bg-black'>Continue</button>
+            </div>
         </div>
     </div>
   )
--- frontend/src/admin/JobList.js
+++ frontend/src/admin/JobList.js
@@ -5,8 +5,6 @@ const JobList = () => {
   return (
     <div>
       <h2>Job Listings</h2>
-
-
       <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -37,13 +35,10 @@ const JobList = () => {
               <td class="px-6 py-4">
                 10/10/2024
               </td>
-
               <td class="px-6 py-4 text-right">
                 <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
               </td>
             </tr>
-
-
           </tbody>
         </table>
       </div>
