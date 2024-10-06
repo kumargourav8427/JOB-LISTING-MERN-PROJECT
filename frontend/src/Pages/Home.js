83f81b786bb43682f69450dbb29029007bedda4d
Pratyush112 <prasaha003@gmail.com>
2024-10-06 11:02:58 +0530

    updated search_bar

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -2,28 +2,14 @@ import React from "react";
 import Job from "./Job";
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import Search from "../component/Search";
 
 function Home() {
   return (
     <>
       <section className="text-gray-600 body-font overflow-hidden">
         <br></br>
-        <div class='max-w-md mx-auto'>
-          <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-            <div class="grid place-items-center h-full w-12 bg-white text-gray-300">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-            </div>
-
-            <input
-              class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
-              type="text"
-              id="search"
-              placeholder="Search here" />
-          </div>
-          <button className="btn btn-neutral">Search</button>
-        </div>
+        <Search />
         <div className="hero min-h-screen">
           <div className="hero-content flex-col lg:flex-row">
             <img
@@ -52,7 +38,6 @@ function Home() {
         </div>
       </section>
       <section>
-        <h1 className="text-3xl font-bold text-center mt-10">Job categories</h1>
         <Job />
       </section>
     </>
